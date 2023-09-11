604c86f596d13991b19f8cd9ab50033bc960f05f
rtrevill <139879271+rtrevill@users.noreply.github.com>
2023-09-12 06:46:15 +1000

    trying to get quiz algorithm functional

--- assets/files/app.js
+++ assets/files/app.js
@@ -1,5 +1,7 @@
 var timeEl = document.getElementById('timer');
-
+var answers = document.querySelectorAll("#options li");
+var H1 = document.querySelector('h1');
+var qBox = document.querySelector('#quiz-box');
 
 function Countdown(){
 
@@ -17,7 +19,6 @@ var timerRunning = setInterval(function() {
 }
 Countdown()
 
-
 var questions = [
 {   Question:  "What color is yellow?",
     Answer1:   "Yellow",
@@ -33,5 +34,22 @@ var questions = [
     Answer4:    "Plover",
     Correct:    "King Parrot"
 }
-
 ]
+
+function askQuestions(){
+    for (i=0; i<questions.length; i++){
+        H1.textContent = questions[i].Question;
+        answers[0].textContent = questions[i].Answer1;
+        answers[1].textContent = questions[i].Answer2;
+        answers[2].textContent = questions[i].Answer3;
+        answers[3].textContent = questions[i].Answer4;
+        qBox.addEventListener('click', function(event){
+            if 
+
+        })
+    }
+}
+
+
+
+askQuestions()
